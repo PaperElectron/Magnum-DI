02d42ec4b644f4c2567ea095d13c4141e834c1eb
Jim Bulkowski <jim.b@paperelectron.com>
2016-01-01 13:12:31 -0500

    Merge method now registers objects if they do not currently exist

--- test/merging.js
+++ test/merging.js
@@ -10,11 +10,12 @@ var should = require('should');
 var injector = require('../index');
 
 describe('Merging contents of a Dependency', function(){
-  context('Merging objects', function() {
+  context('Merging existing objects', function() {
     injector.service('merge', {});
 
     it('Should be a blank object', function() {
       var merge = injector.get('merge');
+      //console.log(merge);
       merge.should.be.an.Object;
       merge.should.be.empty()
     });
@@ -36,5 +37,24 @@ describe('Merging contents of a Dependency', function(){
       merge.name.should.equal('merge');
       merge.location.should.equal('Atlanta')
     });
+  })
+
+  context('Merging a transient object', function(){
+    injector.merge('transient', {name: 'bob'})
+    it('Should be an object', function() {
+      var transient = injector.get('transient');
+      transient.should.be.an.Object
+      transient.should.have.property('name')
+      transient.name.should.equal('bob')
+    });
+
+    it('Should merge a 2nd object', function() {
+      injector.merge('transient', {age: 26});
+      var transient = injector.get('transient');
+      transient.should.be.an.Object;
+      transient.name.should.equal('bob')
+      transient.age.should.equal(26)
+
+    })
   })
 });
